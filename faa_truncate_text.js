d7d760d1d127a3f024bb50028a8a29bb20a6d769
Andrey Fomin <a48671@gmail.com>
2018-06-06 00:30:51 +0500

    fierst commit

---
+++ faa_truncate_text.js
@@ -0,0 +1,48 @@
+jQuery(document).ready(function () {
+
+	console.log('faa_truncate_text - скрипт обрезающий текст');
+	var block = $('.faa_truncate_text');
+
+	block.each(function(i, el) {
+		var curentBlock = $(el);
+		truncateText(curentBlock);
+		
+	});
+
+	function truncateText($el) {
+		var block = $el;
+		var text = block.text();
+		var html = block.html();
+		var lengthText = block.data('length');
+		var readAll = block.data('read_all');
+
+		block.data('text', html);
+
+		var shortText = "";
+
+		for(var i = 0; i < lengthText; i++) {
+			shortText = shortText + text[i];
+		}
+
+		shortText = shortText + ' ...'
+
+		block.text(shortText);
+
+		block.append('<p class = "faa_truncate_text__read_all">' + readAll + '</p>');
+	}
+	
+
+	$('.faa_truncate_text__read_all').click(function() {
+		console.log('click to .faa_truncate_text__read_all');
+		var block = $(this).closest('.faa_truncate_text');
+		block.html(block.data('text'));
+		block.append('<p class = "faa_truncate_text__hidden">Скрыть</p>')
+	});
+
+	$('body').on('click', '.faa_truncate_text__hidden', function() {
+		console.log('click to .faa_truncate_text__hidden');
+		var block = $(this).closest('.faa_truncate_text');
+		truncateText(block);
+	});
+
+})
